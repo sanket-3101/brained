12d832d1464016b0d868977b2fe86d4df4917ec7
Doleravishal <103137585+Doleravishal@users.noreply.github.com>
2022-04-17 13:21:20 +0530

    Sidebar and header for add session

--- src/Pages/Sessions/SessionsAdd.js
+++ src/Pages/Sessions/SessionsAdd.js
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Button, Card, Form, FormControl, InputGroup } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Container,
+  Form,
+  FormControl,
+  InputGroup,
+} from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setLoader, subjectById } from "../../redux/action/SubjectAction";
@@ -7,6 +14,7 @@ import {
   setChapterLoader,
   chapterById,
 } from "../../redux/action/ChapterAction";
+import Dashboard from "../../Component/Sidebar";
 function SessionsAdd(props) {
   let { id } = useParams();
   const dispatch = useDispatch();
@@ -32,71 +40,82 @@ function SessionsAdd(props) {
   console.log("chapter details ===>", chapterDetails);
   return (
     <>
-      <div class="row" className="d-flex ">
-        <div class="col-md-6">
-          <h5 className="ml-5 mt-5">selected Sujects:</h5>
-          <div>
-            <Button className="ml-5 btn-md w-25" variant="info">
-              Add Chapter
-            </Button>
-          </div>
-          <div className="w-25 ml-5 mt-3">
-            <Form.Select
-              onChange={handleChapterChange}
-              aria-label="Default select example"
-            >
-              <option>select chapter</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </Form.Select>
-          </div>
-          <div>
-            <Button className="ml-5 mt-3 btn-md w-25" variant="info">
-              Add Topic
-            </Button>
-          </div>
-          <div className="w-25 ml-5 mt-3">
-            <Form.Select aria-label="Default select example">
-              <option>select Topic</option>
-              {chapterDetails && !cloader ? (
-                <>
-                  {chapterDetails.topics.map((item) => (
-                    <option value="1">{item.name}</option>
-                  ))}
-                </>
-              ) : null}
-            </Form.Select>
-          </div>
-          <div className="w-25 ml-5 mt-3">
-            <InputGroup>
-              <FormControl placeholder="Enter Topic" />
-            </InputGroup>
-          </div>
-        </div>
-        <div class="col-md-6 mt-5">
-          <Card style={{ width: "13rem" }}>
-            <Card.Img
-              variant="top"
-              src="https://source.unsplash.com/user/c_v_r/100x100"
-            />
-            <Card.Body className="bg-info text-center">
-              <Card.Title>AI</Card.Title>
-            </Card.Body>
+      <Dashboard />
+      <Container>
+        <Button className="my-3 btn-lg" variant="info">
+          Sessions{" "}
+        </Button>
+
+        <>
+          <Card style={{ width: "80rem" }} className="">
+            <div class="row" className="d-flex ">
+              <div class="col-md-6">
+                <h5 className="ml-5 mt-5">selected Sujects:</h5>
+                <div>
+                  <Button className="ml-5 btn-md w-25" variant="info">
+                    Add Chapter
+                  </Button>
+                </div>
+                <div className="w-25 ml-5 mt-3">
+                  <Form.Select
+                    onChange={handleChapterChange}
+                    aria-label="Default select example"
+                  >
+                    <option>select chapter</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </div>
+                <div>
+                  <Button className="ml-5 mt-3 btn-md w-25" variant="info">
+                    Add Topic
+                  </Button>
+                </div>
+                <div className="w-25 ml-5 mt-3">
+                  <Form.Select aria-label="Default select example">
+                    <option>select Topic</option>
+                    {chapterDetails && !cloader ? (
+                      <>
+                        {chapterDetails.topics.map((item) => (
+                          <option value="1">{item.name}</option>
+                        ))}
+                      </>
+                    ) : null}
+                  </Form.Select>
+                </div>
+                <div className="w-25 ml-5 mt-3">
+                  <InputGroup>
+                    <FormControl placeholder="Enter Topic" />
+                  </InputGroup>
+                </div>
+              </div>
+              <div class="col-md-6 mt-5">
+                <Card style={{ width: "13rem" }}>
+                  <Card.Img
+                    variant="top"
+                    src="https://source.unsplash.com/user/c_v_r/100x100"
+                  />
+                  <Card.Body className="bg-info text-center">
+                    <Card.Title>AI</Card.Title>
+                  </Card.Body>
+                </Card>
+              </div>
+            </div>
+            <div className="d-flex justify-content-end mb-5">
+              <Button className="" variant="warning">
+                <i class="fas fa-user-tie mr-2 "></i> Start{" "}
+              </Button>
+              <Button className="" variant="primary">
+                <i class="fas fa-user-tie mr-2 "></i> Pause{" "}
+              </Button>
+              <Button className="" variant="primary">
+                <i class="fas fa-user-tie mr-2 "></i> Stop{" "}
+              </Button>
+            </div>
           </Card>
-        </div>
-      </div>
-      <div className="d-flex justify-content-end mb-5">
-        <Button className="" variant="warning">
-          <i class="fas fa-user-tie mr-2 "></i> Start{" "}
-        </Button>
-        <Button className="" variant="primary">
-          <i class="fas fa-user-tie mr-2 "></i> Pause{" "}
-        </Button>
-        <Button className="" variant="primary">
-          <i class="fas fa-user-tie mr-2 "></i> Stop{" "}
-        </Button>
-      </div>
+        </>
+      </Container>
     </>
   );
 }
